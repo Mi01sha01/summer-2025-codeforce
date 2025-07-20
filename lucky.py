3c2bca30de6bc512eeac21df36cd5d435bfc1926
Mi01sha01 <midegaurgesa@gmail.com>
2025-07-20 09:48:58 +0400

    Create lucky.py

---
+++ lucky.py
@@ -0,0 +1,9 @@
+t=int(input())
+for i in range(t):
+    a=list(input())
+    b=int(a[0])+int(a[1])+int(a[2])
+    c=int(a[3])+int(a[4])+int(a[5])
+    if b==c:
+        print("YES")
+    else:
+        print("NO")    
