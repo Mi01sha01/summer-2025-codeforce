f076f9e6903fcf9bc8a69a3b9f93410f6eb5b50a
Mi01sha01 <midegaurgesa@gmail.com>
2025-07-20 12:55:15 +0400

    Create spellCheck.py

---
+++ spellCheck.py
@@ -0,0 +1,12 @@
+t=int(input())
+for i in range(t):
+       n=int(input())
+       a=list(input())
+       a.sort()
+       c=list("Timur")
+       c.sort()
+    
+       if a==c:
+              print('YES')
+       else:
+              print('NO')
