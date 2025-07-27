692c263cebdc2d3e9c1a5fa882e125804962e0cd
Mi01sha01 <midegaurgesa@gmail.com>
2025-07-27 09:00:56 +0400

    Create max_gcd.py

---
+++ max_gcd.py
@@ -0,0 +1,7 @@
+t=int(input())
+for i in range(t):
+    n=int(input())
+ # The maximum possible GCD among all pairs in the range [1, n]
+# is always floor(n / 2).    
+    print(n // 2)
+    # the thing is I didn't come up with this brilliant solution, but stolen.
