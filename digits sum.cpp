f5e02e8f33e51fbbe5937e607ec60fda58da5d32
Mi01sha01 <midegaurgesa@gmail.com>
2025-11-08 10:12:44 +0300

    Create digits sum.cpp

---
+++ digits sum.cpp
@@ -0,0 +1,26 @@
+SourceCopy
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--)
+  {
+    long long x;
+    cin >> x;
+    if (x==9)
+      cout << 1 << endl;
+    else if (x < 9) 
+      cout << 0 << endl;
+      
+    else if (x%10==9)
+      cout << x/10 +1 << endl;
+      
+    else
+      cout << x/10<< endl;  
+        
+  }
+  return 0;
+}
