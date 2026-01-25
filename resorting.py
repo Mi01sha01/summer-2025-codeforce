4b863ee1bb842242a899c553c8739555b64c2e70
Mi01sha01 <midegaurgesa@gmail.com>
2026-01-25 14:55:18 +0300

    Create resorting.py

---
+++ resorting.py
@@ -0,0 +1,22 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())  
+    a = list(map(int, input().split()))
+    
+    b = sorted(a)
+    diff = []
+    
+    if a != b:
+        print(0)
+        
+    else:
+        for i in range(n - 1):
+            c = a[i+1] - a[i]
+            
+            if c % 2 == 0:
+                diff.append((c//2) + 1)
+            
+            else:
+                diff.append((c + 1)//2)   
+                 
+        print(min(diff))
